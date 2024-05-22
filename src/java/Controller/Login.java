14218e9bf33bfdb9363936aeb47fa3f1bd832dc8
huusy24 <synhhe171943@fpt.edu.vn>
2024-05-22 22:26:21 +0700

    first commit

---
+++ src/java/Controller/Login.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller;
+
+import Dao.AccDao;
+import Dao.StudentDao;
+import Model.Student;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author tranm
+ */
+
+public class Login extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Login</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Login at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        request.getRequestDispatcher("Login.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        StudentDao d = new StudentDao();
+        String acc = request.getParameter("account");
+        String id = d.getIdByEmail(acc);
+        Student student = d.getInfoStudent(id);
+        HttpSession session = request.getSession();
+        session.setAttribute("data", student);
+        String login = request.getParameter("login");
+        switch (login) {
+            case "Login":
+                if (student != null && acc.equals(student.getEmail())) {                   
+                    session.setAttribute("email", acc);
+                    request.getRequestDispatcher("Profile.jsp").forward(request, response);
+                } else {
+//                    request.setAttribute("mess", "Wrong account or pass");
+                    request.getRequestDispatcher("Login.jsp").forward(request, response);
+//                    response.sendRedirect("Login.jsp");
+                }
+                break;
+            case "signup":
+                String acc_name = request.getParameter("newEmail");
+                String acc_pass = request.getParameter("acc_pass");
+                String repass = request.getParameter("repass");
+                AccDao accdao = new AccDao();
+                if(repass.equals(acc_pass)){
+                    accdao.insertAcc(acc_name, acc_pass);
+                    request.getRequestDispatcher("Login.jsp").forward(request, response);
+                }else{
+                    request.setAttribute("mess", "pass and repass not connect");
+                    response.sendRedirect("SignUp.jsp");
+                }         
+                break;
+            case "signupPage":
+                request.getRequestDispatcher("SignUp.jsp").forward(request, response);
+                break;
+        }
+
+    }
+
+}
---
+++ web/Login.jsp
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+        <link href="css/login.css" rel="stylesheet" type="text/css"/>
+        <title>Login Form</title>
+    </head>
+    <body>
+  <form class="form-signin" action="login" method="post">
+        <div id="logreg-forms">
+          
+                <h1 class="h3 mb-3 font-weight-normal" style="text-align: center"> Sign in</h1>
+                <p class="text-danger">${mess}</p>
+                <input name="account" type="text" id="inputEmail" class="form-control" placeholder="Account" required="" autofocus="">
+                <input name="pass" type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+
+                <div class="form-group form-check">
+                    <input name="remember" value="1" type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" form="exampleCheck1"> Remember me</label>
+                </div>
+
+                <button class="btn btn-success btn-block" type="submit" name="login" value="Login"><i class="fas fa-sign-in-alt"></i> Sign in</button>
+                <hr>
+                <button class="btn btn-primary btn-block" type="submit" name="login" value="signupPage"><i class="fas fa-user-plus"></i> Sign up New Account</button>
+
+          
+                <br>
+            
+        </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script>
+            function toggleResetPswd(e) {
+                e.preventDefault();
+                $('#logreg-forms .form-signin').toggle()// display:block or none
+                $('#logreg-forms .form-reset').toggle() // display:block or none
+            }
+
+            function toggleSignUp(e) {
+                e.preventDefault();
+                $('#logreg-forms .form-signin').toggle(); // display:block or none
+                $('#logreg-forms .form-signup').toggle(); // display:block or none
+            }
+
+            $(() => {
+                // Login Register Form
+                $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
+                $('#logreg-forms #cancel_reset').click(toggleResetPswd);
+                $('#logreg-forms #btn-signup').click(toggleSignUp);
+                $('#logreg-forms #cancel_signup').click(toggleSignUp);
+            })
+        </script> 
+  </form>
+    </body>
+</html>
