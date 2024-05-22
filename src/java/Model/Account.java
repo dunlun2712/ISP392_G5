8d448212d94012a3016e990174bc10baf2bc8d6d
dunlun2712 <dungtmhe186922@fpt.edu.vn>
2024-05-22 22:55:52 +0700

    add model account

---
+++ src/java/Model/Account.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author tranm
+ */
+public class Account {
+    String acc,pass,role,email,status;
+
+    public Account() {
+    }
+
+    
+    public Account(String acc, String pass, String role, String email, String status) {
+        this.acc = acc;
+        this.pass = pass;
+        this.role = role;
+        this.email = email;
+        this.status = status;
+    }
+
+    public String getAcc() {
+        return acc;
+    }
+
+    public void setAcc(String acc) {
+        this.acc = acc;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+}
