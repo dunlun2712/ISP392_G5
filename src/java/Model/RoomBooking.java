44d01e01fee042239ff308a9bc0bc12c70c5705e
thuhoai03 <143514055+thuhoai03@users.noreply.github.com>
2024-06-11 00:34:34 +0700

    Add files via upload

---
+++ src/java/Model/RoomBooking.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author ASUS
+ */
+public class RoomBooking {
+    
+    private int bookingId;
+    private String roomId;
+    private String usersId;
+    private LocalDate checkinDate;
+    private LocalDate checkoutDate;
+    private BigDecimal totalPrice;
+    private String note;
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getUsersId() {
+        return usersId;
+    }
+
+    public void setUsersId(String usersId) {
+        this.usersId = usersId;
+    }
+
+    public LocalDate getCheckinDate() {
+        return checkinDate;
+    }
+
+    public void setCheckinDate(LocalDate checkinDate) {
+        this.checkinDate = checkinDate;
+    }
+
+    public LocalDate getCheckoutDate() {
+        return checkoutDate;
+    }
+
+    public void setCheckoutDate(LocalDate checkoutDate) {
+        this.checkoutDate = checkoutDate;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+    
+    
+    
+}
