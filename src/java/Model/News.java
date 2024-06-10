d47cce9b3ea77172b718989bcab4f4e2eec94602
NguyenKhanhLyK17 <lynkhe173542@fpt.edu.vn>
2024-06-10 22:45:31 +0700

    add News.java file

---
+++ src/java/Model/News.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author khanh
+ */
+public class News {
+    private int new_id;
+    private String title;
+    private String content;
+    private Date publish_date;
+    private String category;
+}
