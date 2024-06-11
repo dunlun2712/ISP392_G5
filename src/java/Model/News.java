65feaaa4dd7ecc2e1902368f1d7bae36ba7d7f9c
NguyenKhanhLyK17 <lynkhe173542@fpt.edu.vn>
2024-06-11 21:28:40 +0700

    update News.java file

--- src/java/Model/News.java
+++ src/java/Model/News.java
@@ -16,4 +16,77 @@ public class News {
     private String content;
     private Date publish_date;
     private String category;
+    private byte[] image;
+    private String link;
+    public News() {
+    }
+
+    public News(int new_id, String title, String content, Date publish_date, String category, byte[] image, String link) {
+        this.new_id = new_id;
+        this.title = title;
+        this.content = content;
+        this.publish_date = publish_date;
+        this.category = category;
+        this.image = image;
+        this.link = link;
+    }
+
+    public int getNew_id() {
+        return new_id;
+    }
+
+    public void setNew_id(int new_id) {
+        this.new_id = new_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getPublish_date() {
+        return publish_date;
+    }
+
+    public void setPublish_date(Date publish_date) {
+        this.publish_date = publish_date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    
+    
 }
